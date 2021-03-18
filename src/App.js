bafa929b19471a0182b73a463f5f92ec2bd44db5
chandra kiran addagarla <cadda023@uottawa.ca>
2021-03-17 23:16:25 -0400

    created homepage component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import HomePage from './components/homepage';
 
 class App extends Component {
   render() {
     return (
       <div>
-        home page content goes here
+        <HomePage />
       </div>
     );
   }
---
+++ src/components/homepage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HomePage = () =>{  
+    return (
+      <div>
+          <p>new home page content</p>
+      </div>  
+    )
+};
+
+export default HomePage;
