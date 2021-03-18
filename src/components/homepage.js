fe5696c816879e16451b8142155e2ca649d9f682
chandra kiran addagarla <cadda023@uottawa.ca>
2021-03-17 23:43:24 -0400

    checking parallex effect

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Background from './mediaFiles/homepage_background.jpg';
 
 const HomePage = () =>{  
     return (
       <div>
-          <p>new home page content</p>
+          <div class="parallax">
+              <img src={Background}></img>
+          </div>
+          <p>check home page parallex effect on the website hopefully this works as expected.</p>
       </div>  
     )
 };
