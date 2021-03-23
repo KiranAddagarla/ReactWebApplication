7ccc3c14ded08b032c3c265202cb6c92338f67f6
chandra kiran addagarla <cadda023@uottawa.ca>
2021-03-22 21:37:11 -0400

    commit header section

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import HeaderSection from './headerSection'
 import '../App.css';
 
 
 const HomePage = () =>{  
     return (
       <div>
-          
+          <HeaderSection />
           <p>check home page parallex effect on the website hopefully this works as expected.</p>
           
       </div>  
