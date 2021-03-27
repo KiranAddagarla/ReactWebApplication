c6a2e1b0c9f214e559f9a6a40b277c2515cbb269
chandra kiran addagarla <cadda023@uottawa.ca>
2021-03-27 00:46:08 -0400

    commit header section 5

--- src/components/headerContent.js
+++ src/components/headerContent.js
@@ -4,12 +4,12 @@ import '../App.css';
 
 const HeaderContent = () => {
     return(
-        <ul>
+        <ul>s
             <li> <LabelImportantRoundedIcon></LabelImportantRoundedIcon><a href = '#' ><label>ABOUT ME</label></a></li>
-            <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a><label>PROFESSIONAL SUMMARY</label></a></li>
-            <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a><label>TECHNOLOGICAL EXPERTISE</label></a></li>
-            <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a><label>PROJECTS HANDLED</label></a></li>
-            <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a><label>CONTACT ME</label></a></li>
+            <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a href = '#'><label>PROFESSIONAL SUMMARY</label></a></li>
+            <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a href = '#'><label>TECHNOLOGICAL EXPERTISE</label></a></li>
+            <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a href = '#'><label>PROJECTS HANDLED</label></a></li>
+            <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a href = '#'><label>CONTACT ME</label></a></li>
         </ul>
     )
 };
