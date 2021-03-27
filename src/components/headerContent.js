14ae51da8307f7f6274d00400deb56ae64119da2
chandra kiran addagarla <cadda023@uottawa.ca>
2021-03-27 00:34:21 -0400

    commit header section 5

--- src/components/headerContent.js
+++ src/components/headerContent.js
@@ -5,7 +5,7 @@ import '../App.css';
 const HeaderContent = () => {
     return(
         <ul>
-            <li> <LabelImportantRoundedIcon></LabelImportantRoundedIcon><a><label>ABOUT ME</label></a></li>
+            <li> <LabelImportantRoundedIcon></LabelImportantRoundedIcon><a href = {#} ><label>ABOUT ME</label></a></li>
             <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a><label>PROFESSIONAL SUMMARY</label></a></li>
             <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a><label>TECHNOLOGICAL EXPERTISE</label></a></li>
             <li><LabelImportantRoundedIcon></LabelImportantRoundedIcon><a><label>PROJECTS HANDLED</label></a></li>
