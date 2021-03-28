d83db74ba159c056b1958bae1a669ce0b3dba875
chandra kiran addagarla <cadda023@uottawa.ca>
2021-03-28 11:41:20 -0400

    commit about me section 3

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -6,7 +6,7 @@ import '../App.css';
 
 const HomePage = () =>{  
     return (
-      <div>
+      <div className = 'homePage'>
           <HeaderSection />
           <AboutMe />          
       </div>  
