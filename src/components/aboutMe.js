173712a7d21084eb67b7912fa788917a9f49a432
chandra kiran addagarla <cadda023@uottawa.ca>
2021-03-28 11:33:25 -0400

    commit about me section 2

--- src/components/aboutMe.js
+++ src/components/aboutMe.js
@@ -5,6 +5,7 @@ import '../App.css';
 const AboutMe = () => {
     return (
         <div className='AboutMeContainer'>
+            <h3>ABOUT ME</h3>
             <p>
                 Creative and multifaceted Full Stack Web Developer with 3+ years of experience having strong knowledge of Java, JavaScript and keen on solving industry problems. Clear communicator, adept at optimizing processes and debugging technical issues. Passionate technologist keen on incorporating innovative technologies into practice.
             </p>
