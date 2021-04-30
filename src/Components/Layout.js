45e29f45011bea0faae61c30f5989f7fb1d2a2fa
chandra kiran addagarla <cadda023@uottawa.ca>
2021-04-30 18:17:33 -0400

    no navbar scroll changes

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -44,10 +44,10 @@ const Layout = (props) => {
         </div>
       </nav>
       <div data-bs-spy="scroll" data-bs-target="layout-navbar" data-bs-offset="0" tabindex="0">
-        <div id="#HomeContainer" style={{height: "100vh", width: "100vh", justifyContent:"center"}}>
+        <div id="HomeContainer" style={{height: "100vh", width: "100vh", justifyContent:"center"}}>
           <p>Planning to Home body content here</p>
         </div>
-        <div id="#contactMeConatiner"style={{height: "100vh", width: "100vh", justifyContent: "center"}}>
+        <div id="contactMeConatiner"style={{height: "100vh", width: "100vh", justifyContent: "center"}}>
           <p>Planning to ContactMe body content here</p>
         </div>
       </div>
