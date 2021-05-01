d3dca971670ebc2627dc0dac29c59c0c44011742
chandra kiran addagarla <cadda023@uottawa.ca>
2021-05-01 12:20:02 -0400

    cloudinary changes

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -4,7 +4,7 @@ const HomePage = () => {
     return(
         <div id="HomeContainer" className="d-flex justify-content-center primary-section-container home-background">
             <div className="align-self-center">
-                Home Page Content
+                <img src="https://res.cloudinary.com/dz6oeh4ym/image/upload/v1619882372/kiranaddagarla_personal_profile_image.jpg" class="rounded" alt="Kiran's Profle"></img>
             </div>
         </div>
     ) 
