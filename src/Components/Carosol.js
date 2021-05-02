87851ae43fb6a16dd9c22a47c2317dfb407458e8
chandra kiran addagarla <cadda023@uottawa.ca>
2021-05-02 09:26:13 -0400

    basic carosol changes

--- src/Components/Carosol.js
+++ src/Components/Carosol.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Carosol = (props, children) => {
+const Carosol = () => {
     return(
         <div id="carouselExampleIndicators" className="carosol">
             <div class="carousel-indicators">
@@ -21,14 +21,6 @@ const Carosol = (props, children) => {
                     <p>Third carosol screen</p>
                 </div>
             </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
         </div>
         
     )
