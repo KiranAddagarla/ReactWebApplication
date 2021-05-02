fcf4ab2ef67c38e178cf2469fc5cad0d93d9bf8d
chandra kiran addagarla <cadda023@uottawa.ca>
2021-05-01 22:35:10 -0400

    home background overlay changes

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -7,11 +7,11 @@ const HomePage = () => {
     const profileUrl =  "" ;
 
     return(
-        <div id="HomeContainer" className="d-flex justify-content-center primary-section-container home-background">
+        <div id="HomeContainer" className="d-flex justify-content-center primary-section-container home-background home-background-overlay">
             <div className="align-self-center">
             <img src="https://res.cloudinary.com/dz6oeh4ym/image/upload/v1619882372/kiranaddagarla_personal_profile_image.jpg" className="profile-image" alt="Kiran's Profle"></img>
             <h3>Chandra Kiran Addagarla</h3>
-            <h3>I am a full stack developer</h3>
+            <h3>Full Stack Developer</h3>
             </div>
         </div>
     ) 
